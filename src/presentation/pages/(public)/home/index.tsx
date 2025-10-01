a8a6ca30a034af84bb665b6fedd9aad613be32b8
Aly <4ly.alcantara@gmail.com>
2025-10-01 11:03:17 -0300

    feat: enhance home page layout
    - update home component to display user information
    - improve styling for user profile section
    - remove default margin and padding in global styles

--- src/presentation/pages/(public)/home/index.tsx
+++ src/presentation/pages/(public)/home/index.tsx
@@ -1,12 +1,30 @@
 import type {FC} from "react";
 import useGoogleOneTap from "../../../hooks/useGoogleOneTap";
+import {AppParse} from "../../../service/app-parse";
 
 export const Home: FC = () => {
-  useGoogleOneTap(import.meta.env.VITE_GOOGLE_CLIENT_ID, true, false);
+  // useGoogleOneTap(import.meta.env.VITE_GOOGLE_CLIENT_ID, true, false);
+  const user = AppParse.User.current();
+  console.log("current user:", user);
 
   return (
-    <div>
+    <div className="flex items-center justify-center h-screen flex-col gap-4">
       <h1>Home page</h1>
+
+      {user && (
+        <div className="group overflow-hidden flex items-center justify-center bg-black/30 p-3 rounded-full cursor-pointer active:scale-90">
+          {user.get("picture") && (
+            <img
+              className="w-12 rounded-full border-2"
+              src={user.get("picture")}
+              alt="Foto de perfil"
+            />
+          )}
+          <p className="w-0 opacity-0 invisible group-hover:ml-4 !duration-400 text-nowrap group-hover:w-[300px] group-hover:opacity-100 group-hover:visible text-xl">
+            Bem-vindo, {user.get("fullName")}!
+          </p>
+        </div>
+      )}
     </div>
   );
 };
